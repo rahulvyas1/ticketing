c2398549364f91c8479ea7509205573479b80de7
rahul <rahul.class@gmail.com>
2020-07-22 03:28:44 +0530

    removed ssl secure option

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
     // we are on the server
     return axios.create({
       baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+        "http://www.ticket-microservices-app.xyz/",
       headers: req.headers,
 
     });
